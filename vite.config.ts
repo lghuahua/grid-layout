cb82e58ca2e00e79f7c3ea398b9c0ecae96716c7
lghua <877312980@qq.com>
2023-06-16 11:35:51 +0800

    fix: 修改package.json

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     outDir: 'dist',
     lib: {
       entry: resolve(__dirname, 'src/index'),
-      name: 'grid-layout'
+      name: 'grid-layout-vue'
     },
     rollupOptions: {
       external: ['vue'],
